bd8f15cf7d26ca4623b6d7fd97a822ac8b32d9e1
Brett Heithold <baheithold@crimson.ua.edu>
2018-01-28 22:44:18 -0600

    Created dll struct and a private NODE struct.

--- dll.c
+++ dll.c
@@ -4,3 +4,26 @@
  */
 
 #include "dll.h"
+
+typedef struct node {
+    void *value;
+    struct NODE *next;
+    struct NODE *prev;
+} NODE;
+
+NODE *newNODE(void *value, NODE *next, NODE *prev) {
+    NODE *n = malloc(sizeof(NODE));
+    assert(n != 0);
+    n->value = value;
+    n->next = next;
+    n->prev = prev;
+    return n;
+}
+
+struct DLL {
+    NODE *head;
+    int size;
+
+    void (*display)(void *, FILE *);
+    void (*free)(void *);
+};
