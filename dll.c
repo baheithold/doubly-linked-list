36380763dd87319e8155adf01ef1489218944fc1
Brett Heithold <baheithold@crimson.ua.edu>
2018-01-30 00:06:11 -0600

    Started working on insertDLL method.
    Implemented a private addToFront method to take care of inserting values
    at the head of the list.

--- dll.c
+++ dll.c
@@ -20,12 +20,19 @@ NODE *newNODE(void *value, NODE *next, NODE *prev) {
     return n;
 }
 
+// Private DLL method prototypes
+static void addToFront(DLL *items, void *value);
+
 struct DLL {
     NODE *head;
     int size;
 
+    // Public methods
     void (*display)(void *, FILE *);
     void (*free)(void *);
+    
+    // Private methods
+    void (*addToFront)(DLL *, void *);
 };
 
 DLL *newDLL(void (*d)(void *, FILE *), void (*f)(void *)) {
@@ -35,10 +42,29 @@ DLL *newDLL(void (*d)(void *, FILE *), void (*f)(void *)) {
     items->size = 0;
     items->display = d;
     items->free = f;
+    items->addToFront = addToFront;
     return items;
+}
+
+void insertDLL(DLL *items, int index, void *value) {
+    assert(items != 0);
+    assert(index >= 0 && index <= items->size);
 }
 
 int sizeDLL(DLL *items) {
     assert(items != 0);
     return items->size;
+}
+
+
+/************************* Private Methods **************************/
+
+void addToFront(DLL *items, void *value) {
+    assert(items != 0);
+    items->head = newNODE(value, items->head, NULL);
+    if (items->size == 0) {
+        // List was empty before insertion
+        items->tail = items->head;
+    }
+    items->size++;
 }
